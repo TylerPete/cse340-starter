26dfafa8c7b4d04b32c0a1451f70812e0a9e0352
TylerPete <tpeterson4@byupathway.edu>
2026-01-25 18:48:28 -0600

    MVC implementation learning activity (week 3) completed prior to testing

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,1 +1,9 @@
-//placeholder
+const utilities = require("../utilities/");
+const baseController = {};
+
+baseController.buildHome = async function(req, res) {
+    const nav = await utilities.getNav();
+    res.render("index", {title: "Home", nav});
+}
+
+module.exports = baseController; 
